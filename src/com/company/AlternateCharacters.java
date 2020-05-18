26973220114de4dcc975caf2145dd02931f76014
Ildephonse BIKINO <bikino@Ildephonses-MacBook-Pro.local>
2020-05-18 00:30:51 -0700

    First commit

---
+++ src/com/company/AlternateCharacters.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class AlternateCharacters {
+
+
+
+
+}
+
+
+/*
+You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters.
+To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+
+For example, given the string , remove an  at positions  and  to make  in  deletions.
+
+Function Description
+
+Complete the alternatingCharacters function in the editor below. It must return an integer representing the minimum number of deletions
+to make the alternating string.
+
+alternatingCharacters has the following parameter(s):
+
+s: a string
+
+
+Input Format
+
+The first line contains an integer , the number of queries.
+The next  lines each contain a string .
+
+* */
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,78 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        alternateString(Arrays.asList("AAAA","BBBBB","ABABABAB","BABABA","AAABBB"));
+
+        System.out.println(superReducedString("baab"));
+
+        System.out.println(camelcase("saveChangesInTheEditor"));
+    }
+
+
+    public static void alternateString(List<String> stringList){
+
+
+        stringList.forEach(a->{
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] chars = a.toCharArray();
+            for(int i=0;i<chars.length;i++){
+                if(i+1!=chars.length) {
+                    if (chars[i] == chars[i + 1]) {
+                        stringBuilder.append("" + chars[i]);
+                    }
+                }
+            }
+            System.out.println(stringBuilder.length());
+        });
+    }
+
+    public static int anagram(String str1, String str2){
+
+        StringBuilder stringBuilder1 = new StringBuilder(str1.toLowerCase());
+
+        String str1Sorted = Stream.of(str1.split("")).sorted().collect(Collectors.joining());
+        String str2Sorted = Stream.of(str2.split("")).sorted().collect(Collectors.joining());
+
+        if(str1Sorted.equalsIgnoreCase(str2Sorted)) return 0;
+        int count=0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=0;i<str1Sorted.length();i++){
+// .....
+        }
+        return 0;
+    }
+
+
+    static String superReducedString(String s){
+
+       // StringBuffer str = new StringBuffer(s);
+        for(int i=1;i<s.length();i++){
+
+            if(s.charAt(i-1)==s.charAt(i)){
+                s = s.substring(0,i-1) + s.substring(i+1);
+                i=0;
+            }
+            if(s.isEmpty()){
+                return "Empty String";
+            }
+        }
+
+        return s ;
+    }
+
+    static int camelcase(String s){
+        String[] sArray = s.split("[A-Z]");
+
+        return sArray.length;
     }
 }
